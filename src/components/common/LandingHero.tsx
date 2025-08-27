979fbe0375c7a5eab57f98e5168a242ba0a51515
nikhiln22 <nikhilnhegde@gmail.com>
2025-08-27 19:23:51 +0530

    changed the yaml and the landing page

--- src/components/common/LandingHero.tsx
+++ src/components/common/LandingHero.tsx
@@ -26,7 +26,8 @@ const LandingHero: React.FC<LandingHeroProps> = ({
             </h1>
             <p className="text-lg md:text-xl lg:text-2xl max-w-2xl mx-auto lg:mx-0 mb-8 text-gray-600 leading-relaxed">
               One platform to find the best service professionals for your
-              everyday needs.welcome to the world of the fixify and enjoy the services.
+              everyday needs.welcome to the world of the fixify and enjoy the
+              services.
             </p>
             <Button
               onClick={onGetStarted}
