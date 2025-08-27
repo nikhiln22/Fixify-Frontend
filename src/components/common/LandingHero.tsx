cda37eaf0e3b51553ae860167dded2ba9b53f9a7
nikhiln22 <nikhilnhegde@gmail.com>
2025-08-27 20:16:15 +0530

    changed the landing hero section

--- src/components/common/LandingHero.tsx
+++ src/components/common/LandingHero.tsx
@@ -26,8 +26,7 @@ const LandingHero: React.FC<LandingHeroProps> = ({
             </h1>
             <p className="text-lg md:text-xl lg:text-2xl max-w-2xl mx-auto lg:mx-0 mb-8 text-gray-600 leading-relaxed">
               One platform to find the best service professionals for your
-              everyday needs.welcome to the world of the fixify and enjoy the
-              services.Hope you find soon.come and enjoy.
+              everyday needs.
             </p>
             <Button
               onClick={onGetStarted}
